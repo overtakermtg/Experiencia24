d67333733199f3b3bddf1bdfaae7d6f94bceaabd
Overtaker <angel.overtaker@gmail.com>
2017-12-25 10:39:21 -0300

    Final

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :products
   root 'pages#home'
 
   get 'pages/about'
+
+  get 'products/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
